0a672e689aeaec12feaf48e137b9bdf547d2bcb2
bakhanh113ct <bakhanh113ct@gmail.com>
2021-10-12 12:25:35 +0700

    Thêm màu cho subbar xong

--- Login/UI_Home.cs
+++ Login/UI_Home.cs
@@ -7,28 +7,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using Guna.UI2.WinForms;
 namespace Login
 {
     public partial class UI_Home : Form
     {
         List<Panel> subbar = new List<Panel>();
+        List<Guna2Button> Home_button = new List<Guna2Button>(); 
         public UI_Home()
         {
             InitializeComponent();
-            AddSubbar();
+            AddSubbarandHomeButton();
         }
 
-        private void subbar_Visible(Panel a)
+        private void subbar_Change(Panel panel, Guna.UI2.WinForms.Guna2Button button)
         {
             foreach (Panel x in subbar)
             {
                 x.Visible = false;
             }
-            a.Visible = true;
+            foreach (Guna2Button x in Home_button)
+            {
+                x.ForeColor = Color.Black;
+            }
+            panel.Visible = true;
+            button.ForeColor = Color.Fuchsia;
         }
 
-        private void AddSubbar()
+        private void AddSubbarandHomeButton()
         {
             subbar.Add(subbar1);
             subbar.Add(subbar2);
@@ -36,7 +42,12 @@ namespace Login
             subbar.Add(subbar4);
             subbar.Add(subbar5);
             subbar.Add(subbar6);
-
+            Home_button.Add(HOME);
+            Home_button.Add(STORE);
+            Home_button.Add(PARTNER);
+            Home_button.Add(CUSTOMER);
+            Home_button.Add(ANALYSIS);
+            Home_button.Add(SETTING);
         }
         private void exit_Click(object sender, EventArgs e)
         {
@@ -50,32 +61,32 @@ namespace Login
 
         private void HOME_Click(object sender, EventArgs e)
         {
-            subbar_Visible(subbar1);
+            subbar_Change(subbar1, HOME);
         }
 
         private void STORE_Click(object sender, EventArgs e)
         {
-            subbar_Visible(subbar2);
+            subbar_Change(subbar2, STORE);
         }
 
         private void PARTNER_Click(object sender, EventArgs e)
         {
-            subbar_Visible(subbar3);
+            subbar_Change(subbar3, PARTNER);
         }
 
         private void CUSTOMER_Click(object sender, EventArgs e)
         {
-            subbar_Visible(subbar4);
+            subbar_Change(subbar4, CUSTOMER);
         }
 
         private void ANALYSIS_Click(object sender, EventArgs e)
         {
-            subbar_Visible(subbar5);
+            subbar_Change(subbar5, ANALYSIS);
         }
 
         private void SETTING_Click(object sender, EventArgs e)
         {
-            subbar_Visible(subbar6);
+            subbar_Change(subbar6, SETTING);
         }
 
         private void facebook_Click(object sender, EventArgs e)
